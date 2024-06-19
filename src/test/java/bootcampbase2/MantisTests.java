029d9041548dd635f320093ddad0856bd1828034
Rausch <drerausch@gmail.com>
2024-06-19 17:46:31 -0300

    v0.2

--- src/test/java/bootcampbase2/MantisTests.java
+++ src/test/java/bootcampbase2/MantisTests.java
@@ -1,4 +1,23 @@
 package bootcampbase2;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class MantisTests {
+
+    @Test
+    public void criarTarefaComSucesso(){
+        String urlSistema = "https://mantis-prova.base2.com.br/login_page.php";
+        String usuario = "grupoAzulAgua";
+        String senha = "123456";
+
+        //Abrir o Chrome
+        WebDriver chrome = new ChromeDriver();
+
+        //Navegar para o Mantis
+        chrome.navigate().to(urlSistema);
+
+    }
+
 }
