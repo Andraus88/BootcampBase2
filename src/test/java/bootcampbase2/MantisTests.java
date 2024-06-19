09907ef52fddb9593264b49ed0c1f3e0f3b0252c
Rausch <drerausch@gmail.com>
2024-06-19 18:02:17 -0300

    v0.3

--- src/test/java/bootcampbase2/MantisTests.java
+++ src/test/java/bootcampbase2/MantisTests.java
@@ -1,7 +1,9 @@
 package bootcampbase2;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MantisTests {
@@ -12,11 +14,23 @@ public class MantisTests {
         String usuario = "grupoAzulAgua";
         String senha = "123456";
 
-        //Abrir o Chrome
+        //Abrir o Chrome e Navega para o mantis
         WebDriver chrome = new ChromeDriver();
-
-        //Navegar para o Mantis
         chrome.navigate().to(urlSistema);
+
+        //Localiza Username, Entra Usuario e click Entrar
+        WebElement entrarField = chrome.findElement(By.id("username"));
+        entrarField.sendKeys(usuario);
+        WebElement entrarButton = chrome.findElement(By.xpath("//*[@value='Entrar']"));
+        entrarButton.click();
+
+        //Localiza Senha, Entra Senha e click Login
+        WebElement senhaField = chrome.findElement(By.id("password"));
+        senhaField.sendKeys(senha);
+        entrarButton = chrome.findElement(By.xpath("//*[@value='Entrar']"));
+        entrarButton.click();
+
+
 
     }
 
